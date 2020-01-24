bf3648e10b8c035883123e5afdcaaef1c174b9f8
maddy0139 <mahendra0139@gmail.com>
2020-01-25 01:14:54 +0530

    Update database.config.js

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,3 @@
 module.exports = {    
-    url: 'mongodb://maddy:Unix11!@' + process.env.MONGODB_SERVICE_HOST + ':' + process.env.MONGODB_SERVICE_PORT + '/crafting-portal'
+    url: 'mongodb://maddy:Unix11!@' + process.env.CRAFTING_DB_SERVICE_HOST + ':' + process.env.MONGODB_SERVICE_PORT + '/crafting-portal'
 }
