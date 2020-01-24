188c8a5c6923e7a3a0833ecef6d7e874616d39e6
maddy0139 <mahendra0139@gmail.com>
2020-01-25 00:34:27 +0530

    Update database.config.js

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" + process.env.OPENSHIFT_MONGODB_DB_HOST + 'crafting-portal'
+    url: 'mongodb://'+ process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" + process.env.OPENSHIFT_MONGODB_DB_HOST + '/crafting-portal'
+    
 }
