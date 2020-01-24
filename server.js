ba73324fd29ecb54df57eee614e9a7bbb8494ec8
maddy0139 <mahendra0139@gmail.com>
2020-01-25 01:17:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 mongoose.Promise = global.Promise;
-console.log('dbConfig.url', process.env);
+console.log('==============================================================================================');
+console.log('======================================Starting the Application================================');
+console.log('==============================================================================================');
+console.log('===DB Connection String===', dbConfig.url);
+console.log('Environment Variables: ', process.env);
 mongoose.connect(dbConfig.url, {
     useNewUrlParser: true
 }).then(() => {
